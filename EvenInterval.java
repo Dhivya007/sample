2258a9749a3ef5660a8dd9234577accdd85bbfdf
Dhivya007 <slsdhivya@gmail.com>
2016-09-01 21:36:44 +0530

    Add files via upload

---
+++ EvenInterval.java
@@ -0,0 +1,25 @@
+package com.wipro.logical;
+
+import java.util.Scanner;
+
+public class EvenInterval
+{ 
+	public static void main(String[] args)
+	{ 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the starting limit");
+		int start=sc.nextInt();
+		System.out.println("enter the ending limit");
+		int end=sc.nextInt();
+		for (int i = start; i <= end; i++)
+		{
+			if((i%2)==0)
+			{
+				System.out.println(i);
+			}
+			
+		}
+		sc.close();
+	}
+
+}
