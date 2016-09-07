72374aee133ad317f84c102fb6051f40a03d99e6
Dhivya007 <slsdhivya@gmail.com>
2016-09-07 21:40:02 +0530

    Add files via upload

---
+++ HelloPrint.java
@@ -0,0 +1,14 @@
+package com.wipro.logical;
+
+public class HelloPrint
+{
+    public static void main(String[] args)
+    {
+    	for (int i = 0; i < 5; i++) 
+    	{
+			System.out.println("hello");
+		}
+		
+	}
+
+}
